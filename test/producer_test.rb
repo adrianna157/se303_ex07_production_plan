69b8a4986f04c61054739268d397efe3025613cb
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-15 23:25:42 -0700

    test for when a producer is empty

--- test/producer_test.rb
+++ test/producer_test.rb
@@ -2,7 +2,23 @@ gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/producer'
+require_relative '../lib/province'
 
 class ProducerTest < Minitest::Test
+    DATA = {
+            name: 'No producers',
+            producers: [],
+            demand: 30,
+            price: 20
+        }
+    
+    def setup
+        @noProducers = Province.new(DATA)
+    end
+
+    def test_production_shortfall
+        assert_equal(30, @noProducers.shortfall)
+    end
+
 
 end
